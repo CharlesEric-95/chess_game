51a196f9cb81cb8183885bbe1d2591e311063a9a
= <cecallaud92@gmail.com>
2018-10-15 16:04:43 +0200

    Pieces move according to the rules (special move like castle not yet included)

--- direction.py
+++
@@ -1,9 +0,0 @@
-#Python version : 3.7
-#Author : Charles-Eric Callaud
-
-class Direction:
-    #Class describing available moves for a chess piece.
-    def __init__(self, vectors, repeat):
-        self.vectors = vectors
-        self.repeat = repeat
-            
